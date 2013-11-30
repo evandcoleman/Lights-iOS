a4490fc025f16cccaae5dc515855c6343686d620
Evan Coleman <evan@evancoleman.net>
2013-11-30 18:22:58 -0500

    version bump

--- Lights/LTAppDelegate.m
+++ Lights/LTAppDelegate.m
@@ -38,7 +38,7 @@
     UINavigationController *settingsViewController = [[UINavigationController alloc] initWithRootViewController:[[LTSettingsViewController alloc] init]];
     settingsViewController.title = @"Settings";
     settingsViewController.tabBarItem.image = [UIImage imageNamed:@"gear"];
-    self.tabBarController.viewControllers = @[x10ViewController, colorViewController, settingsViewController];
+    self.tabBarController.viewControllers = @[x10ViewController, /*colorViewController,*/ settingsViewController];
     
     self.window.rootViewController = self.tabBarController;
     
@@ -57,6 +57,7 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    [self.session suspendSession];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -66,10 +67,15 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [self.tabBarController presentViewController:self.loadingViewController animated:NO completion:NULL];
+    [self.session resumeSessionWithCompletion:^{
+        [self.tabBarController dismissViewControllerAnimated:YES completion:NULL];
+    }];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
