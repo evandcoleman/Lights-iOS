57b15c209da22af76f86748260ca23f4ca04cfeb
Evan Coleman <evan@evancoleman.net>
2014-03-21 17:36:41 -0400

    show in range beacons

--- Lights/LTBeaconsViewController.m
+++ Lights/LTBeaconsViewController.m
@@ -9,11 +9,19 @@
 #import "LTBeaconsViewController.h"
 #import "LTAppDelegate.h"
 #import <BlocksKit/UIBarButtonItem+BlocksKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface LTBeaconsViewController ()
+static NSString * const LTBeaconUUID = @"E2D56DB8-DFFB-38D2-B06A-D0F5A71096E0";
+static NSString * const LTBeaconKey = @"LTBeaconKey";
+static NSString * const LTBeaconRegionKey = @"LTBeaconRegionKey";
+
+@interface LTBeaconsViewController () <CLLocationManagerDelegate>
 
 @property (nonatomic, readonly) LKSession *session;
 @property (nonatomic) NSArray *beacons;
+@property (nonatomic) NSMutableDictionary *beaconsDict;
+
+@property (nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -23,33 +31,35 @@
     self = [super initWithStyle:UITableViewStylePlain];
     if (self) {
         self.title = @"Beacons";
+        self.locationManager = [[CLLocationManager alloc] init];
+        self.beaconsDict = [[NSMutableDictionary alloc] init];
     }
     return self;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-	
-    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
-    [refreshControl addTarget:self action:@selector(refresh:) forControlEvents:UIControlEventValueChanged];
-    [self setRefreshControl:refreshControl];
-    
-    [refreshControl beginRefreshing];
-    [self refresh:refreshControl];
     
     UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] bk_initWithBarButtonSystemItem:UIBarButtonSystemItemDone handler:^(id sender) {
         [self dismissViewControllerAnimated:YES completion:NULL];
     }];
     self.navigationItem.rightBarButtonItem = doneButton;
-}
-
-#pragma mark - Interface actions
-
-- (void)refresh:(UIRefreshControl *)sender {
+    
+    self.locationManager.delegate = self;
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:LTBeaconUUID];
+    NSString *identifier = @"net.evancoleman.lights";
     [self.session queryBeaconsWithBlock:^(NSArray *beacons) {
-        self.beacons = beacons;
-        [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:0] withRowAnimation:UITableViewRowAnimationFade];
-        [(UIRefreshControl *)sender endRefreshing];
+        NSMutableArray *b = [NSMutableArray array];
+        for (LKBeacon *beacon in beacons) {
+            NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithObject:beacon forKey:LTBeaconKey];
+            
+            CLBeaconRegion *beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid major:beacon.major minor:beacon.minor identifier:identifier];
+            dict[LTBeaconRegionKey] = beaconRegion;
+            
+            [self.locationManager startRangingBeaconsInRegion:beaconRegion];
+            [b addObject:dict];
+        }
+        self.beacons = b;
     }];
 }
 
@@ -57,12 +67,13 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     // Return the number of sections.
-    return 1;
+    return [self.beaconsDict count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // Return the number of rows in the section.
-    return [self.beacons count];
+    NSArray *sectionValues = [self.beaconsDict allValues];
+    return [[sectionValues objectAtIndex:section] count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -70,17 +81,73 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
     if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
     }
     
-    LKBeacon *beacon = self.beacons[indexPath.row];
-    cell.textLabel.text = beacon.name;
+    NSNumber *sectionKey = [[self.beaconsDict allKeys] objectAtIndex:indexPath.section];
+    CLBeacon *beacon = [[self.beaconsDict objectForKey:sectionKey] objectAtIndex:indexPath.row];
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"LTBeaconRegionKey.major == %@ AND LTBeaconRegionKey.minor == %@", beacon.major, beacon.minor];
+    NSDictionary *dict = [[self.beacons filteredArrayUsingPredicate:predicate] firstObject];
+    LKBeacon *b = dict[LTBeaconKey];
+    
+    cell.textLabel.text = b.name;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"Major: %@, Minor: %@, Acc: %.2fm", beacon.major, beacon.minor, beacon.accuracy];
     
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    NSString *title = nil;
+    NSArray *sectionKeys = [self.beaconsDict allKeys];
+    
+    NSNumber *sectionKey = [sectionKeys objectAtIndex:section];
+    switch([sectionKey integerValue]) {
+        case CLProximityImmediate:
+            title = @"Immediate";
+            break;
+            
+        case CLProximityNear:
+            title = @"Near";
+            break;
+            
+        case CLProximityFar:
+            title = @"Far";
+            break;
+            
+        default:
+            title = @"Unknown";
+            break;
+    }
+    
+    return title;
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region {
+    [self.beaconsDict removeAllObjects];
+    
+    NSArray *unknownBeacons = [beacons filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"proximity = %d", CLProximityUnknown]];
+    if([unknownBeacons count])
+        [self.beaconsDict setObject:unknownBeacons forKey:[NSNumber numberWithInt:CLProximityUnknown]];
+    
+    NSArray *immediateBeacons = [beacons filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"proximity = %d", CLProximityImmediate]];
+    if([immediateBeacons count])
+        [self.beaconsDict setObject:immediateBeacons forKey:[NSNumber numberWithInt:CLProximityImmediate]];
+    
+    NSArray *nearBeacons = [beacons filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"proximity = %d", CLProximityNear]];
+    if([nearBeacons count])
+        [self.beaconsDict setObject:nearBeacons forKey:[NSNumber numberWithInt:CLProximityNear]];
+    
+    NSArray *farBeacons = [beacons filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"proximity = %d", CLProximityFar]];
+    if([farBeacons count])
+        [self.beaconsDict setObject:farBeacons forKey:[NSNumber numberWithInt:CLProximityFar]];
+    
+    [self.tableView reloadData];
 }
 
 #pragma mark - Helpers
