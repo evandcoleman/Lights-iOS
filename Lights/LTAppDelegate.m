8e55dcf4881c7e22d1d699251b6a4fe35283214b
Evan Coleman <evan@evancoleman.net>
2014-03-30 18:20:32 -0400

    Revert "oops, missed a line"
    This reverts commit e33ad11050e7cc90040465264ef8bc904ffd2405.

--- Lights/LTAppDelegate.m
+++ Lights/LTAppDelegate.m
@@ -68,7 +68,7 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
-    
+    [self.tabBarController presentViewController:self.loadingViewController animated:NO completion:NULL];
     [self loginAndOpenSession];
 }
 
